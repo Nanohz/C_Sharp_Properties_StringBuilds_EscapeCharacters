088cdfaea098c7f21847468e6150d933744d30ba
Nanohz <38676579+Nanohz@users.noreply.github.com>
2018-09-24 13:57:20 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace CsharpPropertiesExc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            //string fName = "noah";
+            //fName = fName.ToUpper();
+            //string lName = "Goodman";
+            //string mName = "Exavior";
+
+
+            //Console.WriteLine("Hello," +" "+ fName + " "+ mName + " "+ lName + "." + "\n");
+
+            //StringBuilder newnew = new StringBuilder();
+            //newnew.Append("My name is Noah Goodman \n \t My middle name is Exavior.");
+            //Console.WriteLine(newnew);
+            //Console.ReadLine();
+
+        }
+    }
+}
